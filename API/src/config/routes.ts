45ccba37e45c2b31b0b498fe01c4f794ef4ddf08
Diogo Deconto <dideconto@gmail.com>
2021-04-22 21:08:40 -0300

    Cadastro e listagem de ciclos de pagamento

--- API/src/config/routes.ts
+++ API/src/config/routes.ts
@@ -8,7 +8,7 @@ const cicloController = new CicloController();
 // GET -> Solitar recursos/dados do servidor
 // POST -> Enviar recursos/dados/entidades para o servidor
 router.get("/ciclo/listar", cicloController.listar);
-router.get("/ciclo/listar/:rg/:telefone", cicloController.listarPorId);
+router.get("/ciclo/listar/:id", cicloController.listarPorId);
 router.post("/ciclo/cadastrar", cicloController.cadastrar);
 
 export { router };
